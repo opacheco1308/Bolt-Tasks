a40bf6de2ab0bd0b0030c2e3c0ca1f060a26bde3
opacheco1308 <106573610+opacheco1308@users.noreply.github.com>
2022-08-29 16:01:37 -0400

    Add files via upload

---
+++ Refresh_Updates_v1.0.ps1
@@ -0,0 +1,9 @@
+﻿#[system.windows.messagebox]::show('click to continue updates detection,if no update are available the Server will logoff automatically')
+$updateSession = new-object -com "Microsoft.Update.Session"; $updates=$updateSession.CreateupdateSearcher().Search($criteria).Updates
+$i =0
+do{
+$i++
+wuauclt.exe /detectnow
+wuauclt.exe /reportnow
+}
+until ($i -le 10)
---
+++ reset_components.ps1
@@ -0,0 +1,39 @@
+﻿#Resets the Windows Update components
+#DESCRIPTION 
+#This script will reset all of the Windows Updates components to defaults.
+
+ 
+Write-Host "1. Stopping Windows Update Services..."
+Stop-Service -Name wuauserv
+ 
+Write-Host "2. Renaming the Software Distribution..."
+
+$Foldername1="C:\Windows\SoftwareDistribution"
+$Foldername2="C:\Windows\SoftwareDistribution.bak"
+If (Test-Path $Foldername2) 
+{
+Remove-Item -Recurse -Force $Foldername2 }
+else
+ {
+Rename-item $Foldername1 -NewName $Foldername2 }
+ 
+Write-Host "3. Removing old Windows Update log..."
+Remove-Item -Path C:\Windows\WindowsUpdate.log -Force  
+ 
+Write-Host "4. Removing WSUS client registry settings..."
+reg Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientId /f  
+reg Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientIDValidation /f 
+ 
+Write-Host "5. Resetting the WinSock..."
+netsh winsock reset
+ 
+ 
+Write-Host "6. Starting Windows Update Services..."
+Start-Service -Name wuauserv
+
+ 
+Write-Host "7. Forcing discovery..."
+wuauclt /resetauthorization /detectnow
+ 
+Write-Host "Process complete. Server will reboot, please check WSUS Server"
+Restart-Computer -force
